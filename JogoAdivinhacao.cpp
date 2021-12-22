e672be0ef268220ae961940602f99d74c827b881
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-12-22 19:03:04 -0300

    Add files via upload

---
+++ JogoAdivinhacao.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "*************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhacao *" << endl;
+    cout << "*************************************" << endl;
+}
